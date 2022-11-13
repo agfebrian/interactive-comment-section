4457049610b7da0be0360b5b59d62a14458eb380
agfebrian <febriananggi9@gmail.com>
2022-11-13 20:59:13 +0700

    add component button app

---
+++ src/components/Button.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup></script>
+
+<template>
+  <button class="btn">
+    <slot />
+  </button>
+</template>
+
+<style scoped>
+.btn {
+  background-color: var(--color-primary-blue);
+  border: none;
+  border-radius: 5px;
+  color: white;
+  padding: 10px 20px;
+}
+</style>
